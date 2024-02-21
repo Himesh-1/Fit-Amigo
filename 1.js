e9591de10e073fe1d1f435837dd0ec49f26cac16
Himesh-1 <158604351+Himesh-1@users.noreply.github.com>
2024-02-21 08:12:20 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -105,7 +105,7 @@ $(document).ready(function() {
             var recipeHtml = `
                 <div class="grid grid-cols-1 grid-rows-[repeat(1,minmax(0,1fr))]  mb-4">
                     <div class="card bg-[#CDFADB] mx-5 px-4 pt-4 pb-10 my-3 rounded-3xl">
-                        <div class="card-body">
+                        <div class="card-body bg-[#CDFADB]">
                             <h5 class="card-title font-bold underline">${exercise.name}</h5>
                             </br>
                             <p class="card-text"><span class="font-bold underline">Ingredients:</span> ${exercise.type}</p>
